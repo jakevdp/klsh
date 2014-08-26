eb49aa99ef60f43ca0abdc3dba7d9a76f470641a
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-08-26 11:42:13 -0700

    add precomputed fft

--- klsh/kernels.py
+++ klsh/kernels.py
@@ -2,7 +2,53 @@ import numpy as np
 from scipy import fftpack, signal
 
 
-def pairwise_correlate(X, Y, mode='full', fast=True):
+def pairwise_correlate_slow(X, Y, mode='full'):
+    X, Y = map(np.atleast_2d, (X, Y))
+    assert X.ndim == 2
+    assert Y.ndim == 2
+    
+    Y = Y[:, ::-1]
+
+    # Kept for testing purposes
+    first_result = signal.fftconvolve(X[0], Y[0], mode)
+    M = np.zeros((X.shape[0], Y.shape[0], len(first_result)),
+                 dtype=first_result.dtype)
+    
+    for i in range(X.shape[0]):
+        for j in range(Y.shape[0]):
+            M[i, j] = signal.fftconvolve(X[i], Y[j], mode)
+    return M
+    
+
+def precompute_fft(X, Y):
+    """Pre-compute the FFT of X and Y for use in pairwise_correlate"""
+    X, Y = map(np.atleast_2d, (X, Y))
+    assert X.ndim == 2
+    assert Y.ndim == 2
+
+    Y = Y[:, ::-1]
+
+    s1 = X.shape[1]
+    s2 = Y.shape[1]
+    size = s1 + s2 - 1
+    complex_result = (np.issubdtype(X.dtype, np.complex) or
+                      np.issubdtype(Y.dtype, np.complex))
+
+    # Always use 2**n-sized FFT
+    fsize = [int(2 ** np.ceil(np.log2(size)))]
+
+    if not complex_result:
+        X_fft = np.fft.rfftn(X, fsize)
+        Y_fft = np.fft.rfftn(Y, fsize)
+    else:
+        X_fft = np.fft.fftn(X, fsize)
+        Y_fft = np.fft.fftn(Y, fsize)
+
+    return X_fft, Y_fft, (complex_result, size, fsize)
+
+
+def pairwise_correlate(X, Y, mode='full', fast=True,
+                       fft_precomputed=False, fft_info=None):
     """
     Parameters
     ----------
@@ -10,6 +56,19 @@ def pairwise_correlate(X, Y, mode='full', fast=True):
         Two-dimensional arrays to convolve
     mode: string
         ["full"|"valid"|"same"]
+
+    Other Parameters
+    ----------------
+    fast : bool
+        if True (default) use a fast broadcasting-based algorithm.
+        This is mainly for unit-testing purposes.
+    fft_precomputed : bool
+        if True, then X and Y actually contain the pre-computed FFT 
+        of X and Y. Default is False. Cannot be used with fast=False.
+        FFTs can be precomputed with the precompute_fft() function.
+        If True, then complex_result must be specified.
+    fft_info : bool
+        Required if fft_precomputed==True.
         
     Returns
     -------
@@ -17,53 +76,44 @@ def pairwise_correlate(X, Y, mode='full', fast=True):
         Three-dimensional array. out[i, j] contains the
         convolution of X[i] and Y[j]
     """
-    X, Y = map(np.atleast_2d, (X, Y))
-    
-    Y = Y[:, ::-1]
-    
+    if not fast:
+        if fft_precomputed:
+            raise ValueError("Cannot have fft_precomputed and not fast")
+        return pairwise_correlate_slow(X, Y, mode)
+
     if mode != 'full':
         raise NotImplementedError()
-    
-    assert X.ndim == 2
-    assert Y.ndim == 2
-    
-    if fast:
-        s1 = X.shape[1]
-        s2 = Y.shape[1]
-        size = s1 + s2 - 1
-        complex_result = (np.issubdtype(X.dtype, np.complex) or
-                          np.issubdtype(Y.dtype, np.complex))
 
-        # Always use 2**n-sized FFT
-        fsize = [int(2 ** np.ceil(np.log2(size)))]
-
-        if not complex_result:
-            X_fft = np.fft.rfftn(X, fsize)
-            Y_fft = np.fft.rfftn(Y, fsize)
-            M = np.fft.irfftn(X_fft[:, np.newaxis, :] * Y_fft[np.newaxis, :, :],
-                              fsize)[:, :, :size].real
-        else:
-            X_fft = np.fft.fftn(X, fsize)
-            Y_fft = np.fft.fftn(Y, fsize)
-            M = np.fft.ifftn(X_fft[:, np.newaxis, :] * Y_fft[np.newaxis, :, :],
-                             fsize)[:, :, :size]
-
-        #if mode == "full":
-            #pass
-        #elif mode == "same":
-            #return _centered(ret, s1)
-        #elif mode == "valid":
-            #return _centered(ret, s1 - s2 + 1)
-        
+    if fft_precomputed:
+        if fft_info is None:
+            raise ValueError("must specify complex_result=[True/False] "
+                             "if fft_precomputed is True")
+        X_fft, Y_fft = np.asarray(X), np.asarray(Y)
     else:
-        # Kept for testing purposes
-        first_result = signal.fftconvolve(X[0], Y[0], mode)
-        M = np.zeros((X.shape[0], Y.shape[0], len(first_result)),
-                     dtype=first_result.dtype)
-    
-        for i in range(X.shape[0]):
-            for j in range(Y.shape[0]):
-                M[i, j] = signal.fftconvolve(X[i], Y[j], mode)
+        X_fft, Y_fft, fft_info = precompute_fft(X, Y)
+
+    complex_result, size, fsize = fft_info
+
+    assert X_fft.ndim == 2
+    assert Y_fft.ndim == 2
+    assert X_fft.shape[-1] == Y_fft.shape[-1]
+
+    # prepare broadcasting
+    X_fft = X_fft[:, np.newaxis, :]
+    Y_fft = Y_fft[np.newaxis, :, :]
+
+    if not complex_result:
+        M = np.fft.irfftn(X_fft * Y_fft, fsize)[:, :, :size].real
+    else:
+        M = np.fft.ifftn(X_fft * Y_fft, fsize)[:, :, :size]
+
+    #if mode == "full":
+        #pass
+    #elif mode == "same":
+        #return _centered(ret, s1)
+    #elif mode == "valid":
+        #return _centered(ret, s1 - s2 + 1)
+
     return M
 
 
--- klsh/tests/test_kernels.py
+++ klsh/tests/test_kernels.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_allclose
 
-from ..kernels import pairwise_correlate
+from ..kernels import pairwise_correlate, precompute_fft
 
 
 def test_pairwise_correlate(random_seed=0):
@@ -12,11 +12,12 @@ def test_pairwise_correlate(random_seed=0):
 
     def test_correlate(mode, cplx):
         if cplx:
-            p1 = pairwise_correlate(X, Ycomplex, mode=mode, fast=True)
-            p2 = pairwise_correlate(X, Ycomplex, mode=mode, fast=False)
+            Y = Ycomplex
         else:
-            p1 = pairwise_correlate(X, Yreal, mode=mode, fast=True)
-            p2 = pairwise_correlate(X, Yreal, mode=mode, fast=False)
+            Y = Yreal
+
+        p1 = pairwise_correlate(X, Y, mode=mode, fast=True)
+        p2 = pairwise_correlate(X, Y, mode=mode, fast=False)
         assert_allclose(p1, p2)
 
     for mode in ['full']:
@@ -24,3 +25,22 @@ def test_pairwise_correlate(random_seed=0):
             yield test_correlate, mode, cplx
         
 
+def test_pairwise_precomputed(random_seed=0):
+    rng = np.random.RandomState(random_seed)
+    X = rng.rand(15, 60)
+    Yreal = rng.rand(25, 60)
+    Ycomplex = Yreal + 1j * rng.rand(*Yreal.shape)
+
+    def test_correlate(mode, cplx):
+        if cplx:
+            Y = Ycomplex
+        else:
+            Y = Yreal
+
+        Xfft, Yfft, fft_info = precompute_fft(X, Y)
+        p1 = pairwise_correlate(X, Y, mode=mode)
+        p2 = pairwise_correlate(Xfft, Yfft, mode=mode, fft_info=fft_info)
+
+    for mode in ['full']:
+        for cplx in [True, False]:
+            yield test_correlate, mode, cplx
