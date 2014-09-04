bac58a4603d6fb3b17fec42fb290af85fb264373
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-09-04 14:27:02 -0700

    add hamming_cdist utility

--- klsh/tests/test_utils.py
+++ klsh/tests/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.testing import assert_allclose
-from ..utils import hamming_hashes, packbits_axis, unpackbits_axis
+from ..utils import hamming_hashes, packbits_axis, unpackbits_axis, hamming_cdist
 
 def test_hamming_hashes():
     assert_allclose(list(hamming_hashes(0, 3)),
@@ -9,8 +9,25 @@ def test_hamming_hashes():
                     [1, 0, 3, 5, 2, 4, 7, 6])
 
 
+def test_hamming_cdist():
+    def check_hamming_cdist(N, nbits):
+        X = np.random.randint(2, size=(N, nbits))
+        x = packbits_axis(X)
+
+        Y = np.random.randint(2, size=(N, nbits))
+        y = packbits_axis(Y)
+
+        H1 = hamming_cdist(x, y, use_broadcasting=True)
+        H2 = hamming_cdist(x, y, use_broadcasting=False)
+        assert_allclose(H1, H2)
+
+    for N in [50, 100, 200]:
+        for nbits in [4, 8, 12]:
+            yield check_hamming_cdist, N, nbits    
+
+
 def test_packbits_axis():
-    X = np.random.randint(2, size=(4, 5, 6, 7))
+    X = np.random.randint(2, size=(7, 8, 9, 10))
 
     def check_round_trip(axis):
         x = packbits_axis(X, axis)
--- klsh/utils.py
+++ klsh/utils.py
@@ -88,6 +88,60 @@ def unpackbits_axis(x, axis=-1, axissize=None):
     return np.rollaxis(X, -1, axis)
 
 
+def hamming_cdist(x, y=None, use_broadcasting=False):
+    """Compute the matrix of hamming distances between x and y, which are
+    stored in packed-bit format.
+
+    Parameters
+    ----------
+    x, y: nd_arrays
+        x and y should be one-dimensional structured arrays with data type
+        made of some number of unsigned integers.
+    """
+    # TODO: make work with types other than uint8? maybe not needed.
+    assert x.ndim == 1
+
+    if len(x.dtype) > 0:
+        nbytes = len(x.dtype)
+        assert all(x.dtype[i] == np.uint8 for i in range(nbytes))
+    else:
+        nbytes = 1
+        assert x.dtype == np.uint8
+
+    if y is None:
+        y = x
+    else:
+        assert y.ndim == 1
+        assert y.dtype == x.dtype
+
+    if use_broadcasting:
+        x_ints = np.ndarray((x.shape[0], nbytes),
+                            dtype=np.uint8,
+                            buffer=x.data)
+        if y is x:
+            y_ints = x_ints
+        else:
+            y_ints = np.ndarray((y.shape[0], nbytes),
+                                dtype=np.uint8,
+                                buffer=y.data)
+
+        nonmatch_matrix = np.bitwise_xor(x_ints[:, np.newaxis, :],
+                                         y_ints[np.newaxis, :, :])
+
+        res = np.unpackbits(nonmatch_matrix[:, :, :, None], -1).sum((2, 3))
+    else:
+        if len(x.dtype) > 0:
+            it = (np.unpackbits(np.bitwise_xor(x[d][:, None],
+                                               y[d])[:, :, None], -1).sum(-1)
+                  for d in x.dtype.names)
+            res = sum(it)
+        else:
+            res = np.unpackbits(np.bitwise_xor(x[:, None],
+                                               y)[:, :, None], -1).sum(-1)
+
+    return res
+
+
 def hamming_hashes(hashval, nbits, nmax=None):
     """Return an iterator over all (integer) hashes,
     in order of hamming distance
